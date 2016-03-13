cfd1846ed56c39177d755e21dae70c7259beee91
Jason Burns <admiralmudkip@gmail.com>
2016-03-12 22:21:06 -0800

    Initial Commit, a and d keys to rotate lock work

---
+++ Assets/Scripts/LockTurn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockTurn : MonoBehaviour {
+
+    public float speed = 90;
+    public float resetspeed = 120;
+    
+    void Start ()
+    {
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A) && (transform.eulerAngles.z < 90 || transform.eulerAngles.z > 359))
+        {
+            transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) && (transform.eulerAngles.z > 270 || transform.eulerAngles.z < 1))
+        {
+            transform.Rotate(Vector3.back, speed * Time.deltaTime);
+        }
+        else if (transform.eulerAngles.z < 359 && transform.eulerAngles.z >= 269)
+        {
+            transform.Rotate(Vector3.forward, resetspeed * Time.deltaTime);
+        }
+        else if (transform.eulerAngles.z > 1 && transform.eulerAngles.z <= 91)
+        {
+            transform.Rotate(Vector3.back, resetspeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PickMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickMove : MonoBehaviour {
+
+    float x, y;
+	void Start () {
+	    x = Screen.width / 2;
+        y = Screen.height / 3;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //transform.Rotate(new Vector3(x, y), Mathf.Tan((x / Input.mousePosition.x)));
+        //Debug.Log(Input.mousePosition.x);
+        //Input.mousePosition.x
+    }
+}
