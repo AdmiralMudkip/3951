04c56665cb96f5efa5c4d80331e114a99f873d09
Jason Burns <admiralmudkip@gmail.com>
2016-03-27 01:04:01 -0700

    Buttons on main menu work, switching isn't yet.

---
+++ Assets/Scripts/SingleClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class SingleClick : MonoBehaviour {
+    Button b;
+	// Use this for initialization
+	void Awake () {
+        b = GetComponent<Button>();
+        b.onClick.AddListener(StartSingle);
+	}
+
+    void StartSingle()
+    {
+        Debug.LogError("test");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
