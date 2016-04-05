4e3302226060e03e2833735544775424bfa57bc7
Jason Burns <admiralmudkip@gmail.com>
2016-04-04 20:47:53 -0700

    try that again

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+    float timeLeft = 30;
+    int timerr = 0;
+    Text text;
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.deltaTime != 0)
+            timeLeft -= Time.deltaTime;
+        text.text = "Time remaining: " + timerr;
+    }
+}
